0ca9ea5cbad59a4dd483ebacb69d3ce0d70f73f1
dev-talbi <dev.talbi@gmail.com>
2020-12-22 18:36:53 +0100

    mail 80%

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,27 +4,49 @@ namespace App\Controller;
 
 use App\Entity\Design;
 use App\Entity\Project;
+use App\Form\PortofolioContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $repoProject = $this->getDoctrine()->getRepository(Project::class);
         $projects = $repoProject->findAll();
 
         $designs = $this->getDoctrine()->getRepository(Design::class)
         ->findAll();
+
+        $form = $this->createForm(PortofolioContactType::class);
+        $contact = $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getData())
+                ->to('dev.talbi@gmail.com')
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'mail'=>$contact->get('email')->getData(),
+                    'message'=>$contact->get('message')->getData()
+
+                ]);
+                $mailer->send($email);
+                $this->addFlash('message', 'Votre e-mail a bien été envoyer');
+                return $this->redirectToRoute('home');
+        }
         
         return $this->render('home/index.html.twig', [
             'controller_name' => 'ProjectController',
             'projects' => $projects,
-            'designs'=> $designs
+            'designs'=> $designs,
+            'form' => $form->createView() 
         ]);
     }
 }
